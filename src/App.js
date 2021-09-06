80aed7f8791b8f66f431303a176727199b888cb8
Ben <ben.h@builtbypixel.com>
2021-09-06 15:58:33 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Account from "./components/Account";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Nav />
         <Home />
+        <Account />
+        <Signup />
+        <Login />
       </header>
     </div>
   );
