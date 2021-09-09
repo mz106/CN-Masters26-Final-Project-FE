8b1964879ef3f005e13026e969a47d657df80478
Yussuf101 <yussuf985@live.co.uk>
2021-09-09 14:33:08 +0100

    navbar/footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,90 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Footer.css";
-const Footer = () => {
-  const footerStyle = {
-    color: "white",
-    textDecoration: "none",
-  };
+import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+
+function Footer() {
   return (
-    <footer>
-      <ul className="nav-links">
-        <Link style={footerStyle} to="/about">
-          <li className="footers">ABOUT</li>
-        </Link>
-        <Link style={footerStyle} to="/contact">
-          <li className="footers">CONTACT</li>
-        </Link>
-      </ul>
-    </footer>
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+          </div>
+          <div class='social-icons'>
+            <Link
+              class='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i class='fab fa-facebook-f' />
+            </Link>
+            <Link
+              class='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i class='fab fa-instagram' />
+            </Link>
+            <Link
+              class='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i class='fab fa-youtube' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i class='fab fa-twitter' />
+            </Link>
+            <Link
+              class='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i class='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
   );
-};
+}
 
 export default Footer;
