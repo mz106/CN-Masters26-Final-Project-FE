1fadc7d82d8661d8159549fec7b418c4b92416f4
Michael Z <flashman123@fastmail.fm>
2021-09-16 17:39:05 +0100

    calisthenics and accessories on page

--- src/components/ProductPages/Accessories.js
+++ src/components/ProductPages/Accessories.js
@@ -1,7 +1,71 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import CardContainer from "./CardContainer";
+import fetchProducts from "./functions.js";
 
-function Accessories({ setCartItems }) {
-  return <div>Accessories</div>;
-}
+const Accessories = ({ cartItems, setCartItems }) => {
+  const [page, setPage] = useState(1);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(6);
+  const [products, setProducts] = useState({ count: 0, rows: [] });
+  const cat = "Accessories";
+  const getLastPage = () => Math.ceil((products?.count || 0) / limit);
+
+  useEffect(
+    () => (async () => setProducts(await fetchProducts(limit, offset, cat)))(),
+    [offset]
+  );
+
+  return (
+    <>
+      <div>
+        <CardContainer
+          products={products}
+          cartItems={cartItems}
+          setCartItems={setCartItems}
+        />
+      </div>
+      <div className="pages-buttons">
+        <button
+          onClick={() => {
+            setPage(1);
+            setOffset(0);
+          }}
+        >
+          First
+        </button>
+        <button
+          onClick={() => {
+            setPage((old) => Math.max(old - 1, 1));
+            setOffset(offset - (limit - 1));
+          }}
+        >
+          prev
+        </button>
+        <span>
+          {" "}
+          {page} of {getLastPage()}
+        </span>
+        <button
+          onClick={() => {
+            setPage((old) => (!products || old === 3 ? old : old + 1));
+            setOffset(offset + (limit - 1));
+          }}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => {
+            setPage(getLastPage());
+            setOffset(products.count - (products.count % limit));
+          }}
+        >
+          Last
+        </button>
+      </div>
+    </>
+  );
+};
+
 
 export default Accessories;
